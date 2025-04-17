3a4f0af797e8e21433ec0346b502ed0e087eb7a0
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-04-17 07:49:08 +0300

    add changes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const path = require('path')
-
+const logger = require('./logger')
 const fortune = require('./src/lib/fortune')
 
+// Создаем приложение Express
 const app = express()
 const port = process.env.PORT || 3000
 
+// Настраиваем Handlebars
 app.engine(
 	'handlebars',
 	engine({
@@ -14,33 +16,46 @@ app.engine(
 		extname: '.handlebars',
 	})
 )
-// изменения из mian
 app.set('view engine', 'handlebars')
 app.set('views', path.join(__dirname, 'src', 'views'))
 
+// Логирование запросов
+app.use((req, res, next) => {
+	logger.info({
+		message: 'HTTP Request received',
+		method: req.method,
+		url: req.url,
+		user: { id: '123', name: 'Denis' },
+	})
+
+	next()
+})
+
+// Настраиваем маршруты
 app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) =>
-	// изменения из mian
+app.get('/about', (req, res) => {
+	logger.info({
+		message: 'HTTP Request received',
+		method: req.method,
+		url: req.url,
+		user: { id: '123', name: 'Denis' },
+	})
 
-	// изменения из mian
-
-	// изменения из mian
-
-	// изменения из mian
 	res.render('about', { title: 'About', fortune: fortune.getFortune() })
-)
-
+})
 app.get('*', (req, res) => {
 	res.status(404)
 	res.render('404')
 })
 
+// Логирование ошибок
 app.use((err, req, res, next) => {
+	logger.error({ message: 'Internal Server Error', error: err.message })
 	res.status(500)
 	res.render('500')
 })
 
-// add comment
-app.listen(port, () =>
-	console.log(`Сервер запущен на http://localhost:${port}`)
-)
+// Запуск сервера
+app.listen(port, () => {
+	logger.info(`Сервер запущен на http://localhost:${port}`)
+})
